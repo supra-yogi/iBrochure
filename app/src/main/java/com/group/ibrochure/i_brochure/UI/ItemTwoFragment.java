06a38d454808b04d92990c865de8cc46a37b041c
Aan Ardiyana <aanardiyana2@gmail.com>
2017-11-08 10:39:23 +0700

    add navigation bottom bar in ListBrochure

---
+++ app/src/main/java/com/group/ibrochure/i_brochure/UI/ItemTwoFragment.java
@@ -0,0 +1,36 @@
+package com.group.ibrochure.i_brochure.UI;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group.ibrochure.i_brochure.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ItemTwoFragment extends Fragment {
+
+
+    public static ItemTwoFragment newInstance() {
+        ItemTwoFragment fragment = new ItemTwoFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_two, container, false);
+    }
+
+}
--- app/src/main/java/com/group/ibrochure/i_brochure/UI/ListBrochureActivity.java
+++ app/src/main/java/com/group/ibrochure/i_brochure/UI/ListBrochureActivity.java
@@ -1,11 +1,19 @@
 package com.group.ibrochure.i_brochure.UI;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.group.ibrochure.i_brochure.R;
 import com.srx.widget.PullToLoadView;
@@ -20,16 +28,51 @@ public class ListBrochureActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        PullToLoadView pullToLoadView = (PullToLoadView) findViewById(R.id.pullToLoadView);
+        TextView mTitle = (TextView) findViewById(R.id.toolbar_title);
+
+        Typeface customFont = Typeface.createFromAsset(this.getAssets(), "fonts/pacifico.ttf");
+        mTitle.setTypeface(customFont);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+            actionBar.setDisplayShowTitleEnabled(false);
+
+            PullToLoadView pullToLoadView = (PullToLoadView) findViewById(R.id.pullToLoadView);
         new Paginator(this, pullToLoadView).initializePaginator();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        Navigation Bottom Init
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                selectedFragment = ItemOneFragment.newInstance();
+                                break;
+                            case R.id.navigation_mybrochure:
+                                selectedFragment = ItemTwoFragment.newInstance();
+                                break;
+                            case R.id.navigation_person:
+                                selectedFragment = ItemThreeFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, ItemOneFragment.newInstance());
+        transaction.commit();
+
     }
 }

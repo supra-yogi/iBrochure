04e74fbe3b006ce141b4145090afa89c5990fd5d
Aan Ardiyana <aanardiyana2@gmail.com>
2017-11-10 14:31:08 +0700

    edit nav menu drawer

--- app/src/main/java/com/group/ibrochure/i_brochure/UI/ListBrochureActivity.java
+++ app/src/main/java/com/group/ibrochure/i_brochure/UI/ListBrochureActivity.java
@@ -89,30 +89,4 @@ public class ListBrochureActivity extends AppCompatActivity implements Navigatio
         return true;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            case R.id.action_logout:
-                session.logOut();
-                finish();
-                System.exit(0);
-                break;
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
